fe0693a15eacdbf71fd4f19536deec630f88000f
Antti Garding <>
2024-01-02 15:46:13 +0200

    Add first unit tests
    Add basis for unit testing using Google Test and tests for PhotoFile::get_camera_id()
    
    Update .gitignore with files related to unit testing.

---
+++ test/photo_util_test.cpp
@@ -0,0 +1,13 @@
+#include "../photo_util.h"
+
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(PhotoUtil, get_camera_id) {
+    ASSERT_STREQ(PhotoUtil::get_camera_id("DSC-RX100M3").c_str(), "RXC");
+    ASSERT_STREQ(PhotoUtil::get_camera_id("H8324").c_str(), "XPA");
+    ASSERT_STREQ(PhotoUtil::get_camera_id("abc").c_str(), "");
+}
+
+}
